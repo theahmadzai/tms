615e21c88d7f0f601f3d517583fe6881d75d5c58
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-01-27 00:14:11 +0500

    Remove Type

--- src/immortal/annotations/Column.java
+++ src/immortal/annotations/Column.java
@@ -5,11 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import immortal.constants.Type;
-
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Column {
-	String name();
-	Type type();
+	String value();
 }
